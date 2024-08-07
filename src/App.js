d5f23211dd82140db869dcdced06e81a9865125b
zatrimaylova <m.zatrimaylova@gmail.com>
2024-08-07 20:06:46 +0300

    fixed targets rendering

--- src/App.js
+++ src/App.js
@@ -2,26 +2,28 @@ import React, { useState, useEffect } from "react";
 
 import "./index.scss";
 
-//const COLORS = [];
-
-const Target = (props) => {
-  const cathTarger = () => {};
-
-  return (
-    <div onClick={cathTarger} className="target" style={props.style}></div>
-  );
-};
+const Target = (props) => (
+  <div
+    onClick={() => props.onClick(props.id)}
+    className="target"
+    style={props.style}
+  ></div>
+);
 
 const App = () => {
-  //const [attempts, setAttempts] = useState(3);
+  const [score, setScore] = useState(0);
   const [сomponents, setComponents] = useState([]);
+  const [clickedTarget, setClickedTarget] = useState([]);
 
   const createSize = () => Math.floor(Math.random() * 50) + "px";
 
-  console.log(window);
+  const handleScore = (id) => {
+    setScore((prevScore) => prevScore + 1);
+    setClickedTarget((prevCickedTarget) => [...prevCickedTarget, id]);
+  };
+
   useEffect(() => {
     const intervalComponents = setInterval(() => {
-      console.log("kdejkjdwhdwhjhgdwe");
       const size = createSize();
       setComponents((prevComponents) => [
         ...prevComponents,
@@ -36,11 +38,12 @@ const App = () => {
             width: size,
           }}
           key={prevComponents.length + 1}
+          onClick={handleScore}
+          score={score}
+          id={prevComponents.length + 1}
         />,
       ]);
-    }, 3000);
-
-    console.log(сomponents);
+    }, 300);
 
     return () => {
       // This is the cleanup function
@@ -52,7 +55,11 @@ const App = () => {
 
   return (
     <div className="App">
-      <header className="App-header">{сomponents}</header>
+      <header className="App-header">
+        {сomponents.map((comp) =>
+          clickedTarget.includes(comp.props.id) ? null : comp
+        )}
+      </header>
     </div>
   );
 };
